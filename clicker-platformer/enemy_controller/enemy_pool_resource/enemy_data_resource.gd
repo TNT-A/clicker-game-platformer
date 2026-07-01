d232e84b94b1aee6a3674f4d9c06c2454e06dd67
TNT-A <andrejr.snellings@gmail.com>
2026-07-01 19:49:45 -0400

    Big progress on enemy controller script
    Implemented enemy spawning credits as well as a weighted spawning system for within the enemy controller. In doing so I created the enemy_pool_resource, a resource that allows me to specify then enemies available to spawn in a room, as well as their credit counts and spawn weight. Also added a variable for controlling the chances of an elite enemy spawning, though it's currently unused as elites have not been implemented yet. I'm planning to implement the risk of rain style of enemy spawning where enemies spawn over time or in burst rather than my previous iteration where every enemy spawns at once all at the start.

---
+++ clicker-platformer/enemy_controller/enemy_pool_resource/enemy_data_resource.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name EnemyDataResource
+
+@export var spawn_weight : float = 1.0
+@export var credit_num : float = 1.0
--- clicker-platformer/maps/map_list/starting/starting_room.gd
+++ clicker-platformer/maps/map_list/starting/starting_room.gd
@@ -7,6 +7,8 @@ var room_ended : bool = false
 @export var final_room : bool = false
 @export var starting_room : bool = false
 
+@export var pool_resource : EnemyPoolResource = preload("res://enemy_controller/enemy_pool_resource/pool_testing.tres")
+
 func _on_camera_move_area_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
 		SignalBus.move_camera.emit($CameraPos.global_position)
